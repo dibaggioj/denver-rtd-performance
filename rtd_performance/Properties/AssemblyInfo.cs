84050dfb28e5842c7dae1437cb326703db19cee1
John DiBaggio <john.dibaggio@vml.com>
2017-03-30 18:07:48 -0600

    fix config file

--- rtd_performance/Properties/AssemblyInfo.cs
+++ rtd_performance/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DiBaggio_John_Lab6")]
+[assembly: AssemblyTitle("rtd_performance")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DiBaggio_John_Lab6")]
+[assembly: AssemblyProduct("rtd_performance")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
